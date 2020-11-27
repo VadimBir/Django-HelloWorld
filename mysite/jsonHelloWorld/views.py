5bf97ba2f26467450663309412c7e30cc94c6a29
VadimBir <vasumrus@gmail.com>
2020-11-27 16:51:59 -0600

    JSON response

---
+++ mysite/jsonHelloWorld/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+def index(request):
+    return JsonResponse({'message':'Hello World!'})
+
+
+
+
+# Create your views here.
